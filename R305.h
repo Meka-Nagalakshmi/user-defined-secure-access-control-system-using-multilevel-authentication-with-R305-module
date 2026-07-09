a30644a08dc53d535cf89fa6358a4a25e73c3a79
Meka-Nagalakshmi <nagalakshmimeka2004@gmail.com>
2026-07-10 00:10:53 +0530

    R305.h

---
+++ R305.h
@@ -0,0 +1,12 @@
+#include "types.h"
+void send_packet(u8 *ptr,u8 n);
+s8 collect(void);
+s8 store1(void);
+s8 store2(void);
+s8 gen_template(void);
+s8 store_temp(s8 id);
+s8 delete_fp(s8 id);
+s8 identify_fp(void);
+s8 load_temp(s8 id);
+s8 enroll(s8 id);
+s8 search(s8 id);	
